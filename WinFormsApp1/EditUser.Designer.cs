713059f04e4bf9fafedd2f7865683920f66d17fc
Ilya <svaikinilya@yandex.ru>
2022-12-09 21:50:43 +0300

    Пароль сбрасывается

--- WinFormsApp1/EditUser.Designer.cs
+++ WinFormsApp1/EditUser.Designer.cs
@@ -34,14 +34,15 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,50 +59,43 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(596, 43);
+            this.textBox1.Location = new System.Drawing.Point(609, 43);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(596, 90);
+            this.textBox2.Location = new System.Drawing.Point(609, 90);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(125, 27);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(596, 135);
+            this.textBox3.Location = new System.Drawing.Point(609, 135);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(125, 27);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(596, 182);
+            this.textBox4.Location = new System.Drawing.Point(609, 182);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(125, 27);
             this.textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(596, 233);
+            this.textBox5.Location = new System.Drawing.Point(609, 233);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(125, 27);
             this.textBox5.TabIndex = 5;
             // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(596, 284);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(125, 27);
-            this.textBox6.TabIndex = 6;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(484, 46);
+            this.label1.Location = new System.Drawing.Point(462, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 20);
             this.label1.TabIndex = 7;
@@ -110,7 +104,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(484, 93);
+            this.label2.Location = new System.Drawing.Point(462, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 20);
             this.label2.TabIndex = 8;
@@ -119,7 +113,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(484, 138);
+            this.label3.Location = new System.Drawing.Point(462, 138);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(72, 20);
             this.label3.TabIndex = 9;
@@ -128,7 +122,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(484, 185);
+            this.label4.Location = new System.Drawing.Point(462, 185);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 20);
             this.label4.TabIndex = 10;
@@ -137,24 +131,15 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(484, 236);
+            this.label5.Location = new System.Drawing.Point(462, 236);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(52, 20);
             this.label5.TabIndex = 11;
             this.label5.Text = "Логин";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(484, 287);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(62, 20);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Пароль";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(503, 376);
+            this.button1.Location = new System.Drawing.Point(596, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(189, 62);
             this.button1.TabIndex = 13;
@@ -162,19 +147,46 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 62);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Сбросить пароль";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(462, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Новый пароль";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(609, 302);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.TabIndex = 16;
+            // 
             // EditUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
@@ -198,13 +210,14 @@
         private TextBox textBox3;
         private TextBox textBox4;
         private TextBox textBox5;
-        private TextBox textBox6;
         private Label label1;
         private Label label2;
         private Label label3;
         private Label label4;
         private Label label5;
-        private Label label6;
         private Button button1;
+        private Button button2;
+        private Label label6;
+        private TextBox textBox6;
     }
 }
--- WinFormsApp1/EditUser.cs
+++ WinFormsApp1/EditUser.cs
@@ -47,7 +47,6 @@ namespace WinFormsApp1
             dataGridView1.Columns.Add("n3","Отчество");
             dataGridView1.Columns.Add("n4","Телефон");
             dataGridView1.Columns.Add("n5","Логин");
-            dataGridView1.Columns.Add("n6","Пароль");
             dataGridView1.Columns.Add("n7", "pdId");
             dataGridView1.Columns.Add("n8", "id");
             dataGridView1.Columns[3].Visible = false;
@@ -55,18 +54,109 @@ namespace WinFormsApp1
             dataGridView1.Columns[5].Visible = false;
             dataGridView1.Columns[6].Visible = false;
             dataGridView1.Columns[7].Visible = false;
-            dataGridView1.Columns[8].Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            updatePerson(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text,
+            updatePersonWithoutPwd(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text,
                 dt.Rows[ind]["pdID"].ToString(),
                 dt.Rows[ind]["id"].ToString(), database);
             updateTable();
         }
 
-        private static void updatePerson(string s1, string s2, string s3, string s4, string s5, string s6, string s7, string s8, DataBase database)
+        private  void updatePersonWithoutPwd(string s1, string s2, string s3, string s4, string s5, string s7, string s8, DataBase database)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            string sqlExpression = "updatePersonWithoutPass";
+            SqlCommand command = new SqlCommand(sqlExpression, database.GetSqlConnection());
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlParameter a1 = new SqlParameter
+            {
+                ParameterName = "@Sur",
+                Value = s1
+            };
+            SqlParameter a2 = new SqlParameter
+            {
+                ParameterName = "@Name",
+                Value = s2
+            };
+            SqlParameter a3 = new SqlParameter
+            {
+                ParameterName = "@Fat",
+                Value = s3
+            };
+            SqlParameter a4 = new SqlParameter
+            {
+                ParameterName = "@Ph",
+                Value = s4
+            };
+            SqlParameter a5 = new SqlParameter
+            {
+                ParameterName = "@log",
+                Value = s5
+            };
+            SqlParameter a7 = new SqlParameter
+            {
+                ParameterName = "@pdID",
+                Value = s7
+            };
+            SqlParameter a8 = new SqlParameter
+            {
+                ParameterName = "@userId",
+                Value = s8
+            };
+            command.Parameters.Add(a1);
+            command.Parameters.Add(a2);
+            command.Parameters.Add(a3);
+            command.Parameters.Add(a4);
+            command.Parameters.Add(a5);
+            command.Parameters.Add(a7);
+            command.Parameters.Add(a8);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+        }
+
+        private static DataTable getPersons(DataBase database)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            string sqlExpression = "getPersons";
+            SqlCommand command = new SqlCommand(sqlExpression, database.GetSqlConnection());
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count - 1)
+            {
+                ind = e.RowIndex;
+                textBox1.Text = dt.Rows[e.RowIndex]["Surname"].ToString();
+                textBox2.Text = dt.Rows[e.RowIndex]["Namee"].ToString();
+                textBox3.Text = dt.Rows[e.RowIndex]["Patronymic"].ToString();
+                textBox4.Text = dt.Rows[e.RowIndex]["PhoneNumber"].ToString();
+                textBox5.Text = dt.Rows[e.RowIndex]["login"].ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "")
+            {
+                return;
+            }
+            updatePerson(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text,textBox6.Text,
+                dt.Rows[ind]["pdID"].ToString(),
+                dt.Rows[ind]["id"].ToString(), database);
+            updateTable();
+            MessageBox.Show("Пароль сброшен");
+            textBox6.Text = "";
+        }
+
+        private void updatePerson(string s1, string s2, string s3, string s4, string s5, string s6, string s7, string s8, DataBase database)
         {
             SqlDataAdapter adapter = new SqlDataAdapter();
             DataTable table = new DataTable();
@@ -125,30 +215,5 @@ namespace WinFormsApp1
             adapter.Fill(table);
         }
 
-        private static DataTable getPersons(DataBase database)
-        {
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            DataTable table = new DataTable();
-            string sqlExpression = "getPersons";
-            SqlCommand command = new SqlCommand(sqlExpression, database.GetSqlConnection());
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-            adapter.SelectCommand = command;
-            adapter.Fill(table);
-            return table;
-        }
-
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count - 1)
-            {
-                ind = e.RowIndex;
-                textBox1.Text = dt.Rows[e.RowIndex]["Surname"].ToString();
-                textBox2.Text = dt.Rows[e.RowIndex]["Namee"].ToString();
-                textBox3.Text = dt.Rows[e.RowIndex]["Patronymic"].ToString();
-                textBox4.Text = dt.Rows[e.RowIndex]["PhoneNumber"].ToString();
-                textBox5.Text = dt.Rows[e.RowIndex]["login"].ToString();
-                textBox6.Text = dt.Rows[e.RowIndex]["passwd"].ToString();
-            }
-        }
     }
 }
